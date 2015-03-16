b762a6337a7167e7a0c5993c91c659d8cc5e2ae8
Callum Macrae <callum@macr.ae>
2015-03-16 15:58:07 +0000

    Refactored, fixed linting errors, and added tests

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
+'use strict';
+
 module.exports = {
+    stderr: function hookStderr(callback, addl) {
+        var oldstderr = process.stderr.write;
 
-    stderr: function(callback, addl) {
-        oldstderr = process.stderr.write;
-        process.stderr.write = (function(write) {
-            return function(string, encoding, fd) {
-                if(addl) write.apply(process.stderr, arguments)
-                callback(string, encoding, fd)
-            }
-        })(process.stderr.write)
+        process.stderr.write = function hookedStderrWrite(string, encoding, fd) {
+			if (addl) {
+				oldstderr.apply(process.stderr, arguments);
+			}
+			callback(string, encoding, fd);
+		};
 
-		return function() {
+		return function () {
 			process.stderr.write = oldstderr;
-		}
+		};
     },
 
-    stdout: function(callback, addl) {
-        oldstdout = process.stdout.write;
-        process.stdout.write = (function(write) {
-            return function(string, encoding, fd) {
-                if(addl) write.apply(process.stdout, arguments)
-                callback(string, encoding, fd)
-            }
-        })(process.stdout.write)
+    stdout: function hookStdout(callback, addl) {
+        var oldstdout = process.stdout.write;
 
-		return function() {
+        process.stdout.write = function hookedStdoutWrite(string, encoding, fd) {
+			if (addl) {
+				oldstdout.apply(process.stdout, arguments);
+			}
+			callback(string, encoding, fd);
+		};
+
+		return function () {
 			process.stdout.write = oldstdout;
-		}
+		};
     }
-
 };
-
